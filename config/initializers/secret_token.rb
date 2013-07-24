b282d38e7e88a29105554d0eae6e4f8f9151eb97
Jaime Bunzli <jpbunzli@gmail.com>
2013-07-24 17:59:37 -0400

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PuntoTicketExpress::Application.config.secret_token = 'ea85a94e914d4e2301ba2ccbaa93864bef840e4ac8168ad4f923cfda58e07d470e746c42d66b2d848ee5a46f674336c163e6a9e82bc21319c5e4eab4852a690b'
