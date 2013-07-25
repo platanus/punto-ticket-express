e503dda194f72622ac71540747f375e2151c3022
Jaime Bunzli <jpbunzli@gmail.com>
2013-07-25 15:40:38 -0400

    accepts creation of ticket types on event creation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ActiveRecord::Base
-  attr_accessible :user_id, :address, :custom_url, :description, :name, :organizer_description, :organizer_name
+  attr_accessible :user_id, :address, :custom_url, :description, :name, :organizer_description, :organizer_name, :ticket_types_attributes
 
   belongs_to :user
   has_many :ticket_types
+
+  accepts_nested_attributes_for :ticket_types
 end
