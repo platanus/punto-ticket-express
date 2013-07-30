6cdc3ba38d135ff1cd3240d0d22d4040dd232f80
Jaime Bunzli <jpbunzli@gmail.com>
2013-07-30 17:00:40 -0400

    transform datetime data in controller before save it

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,13 +3,11 @@
 class Event < ActiveRecord::Base
   # attrs
   attr_accessible :user_id, :address, :custom_url, :description, :name, :organizer_description, :organizer_name
-  attr_accessible :ticket_types_attributes, :is_published, :start_data_time, :end_data_time
-  # attrs non persistent
-  attr_accessible :start_date, :start_time, :end_date, :end_time
-  # getters & setters
-  attr_accessor :start_date, :start_time, :end_date, :end_time
+  attr_accessible :ticket_types_attributes, :is_published, :start_time, :end_time
+
   # validations
   validates_presence_of :address, :custom_url, :description, :name, :organizer_name
+
   # relationship
   belongs_to :user
   has_many :ticket_types, :dependent => :destroy
