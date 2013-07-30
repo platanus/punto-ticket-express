c22102803ce6ac37b3ad726acf5549eff844f4c8
Jaime Bunzli <jpbunzli@gmail.com>
2013-07-30 12:43:31 -0400

    revert change to migration of time events

--- db/migrate/20130726141414_adds_times_to_events.rb
+++ db/migrate/20130726141414_adds_times_to_events.rb
@@ -1,6 +1,6 @@
 class AddsTimesToEvents < ActiveRecord::Migration
   def change
-    add_column :events, :start_data_time, :datetime
-    add_column :events, :end_data_time, :datetime
+    add_column :events, :start_time, :datetime
+    add_column :events, :end_time, :datetime
   end
 end
