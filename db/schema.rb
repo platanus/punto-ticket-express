e96e300441cebe8c40cbae15a394eac8f212f857
Jaime Bunzli <jpbunzli@gmail.com>
2013-08-16 18:07:16 -0400

    update schema after last migration

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,24 @@ ActiveRecord::Schema.define(:version => 20130815202327) do
   add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
   add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
 
+  create_table "admin_users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
+  add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
   create_table "events", :force => true do |t|
     t.string   "name"
     t.string   "address"
@@ -35,8 +53,6 @@ ActiveRecord::Schema.define(:version => 20130815202327) do
     t.string   "organizer_name"
     t.text     "organizer_description"
     t.string   "custom_url"
-    t.datetime "start"
-    t.datetime "end"
     t.datetime "created_at",                               :null => false
     t.datetime "updated_at",                               :null => false
     t.integer  "user_id"
@@ -64,8 +80,8 @@ ActiveRecord::Schema.define(:version => 20130815202327) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",            :null => false
-    t.string   "encrypted_password",     :default => "",            :null => false
+    t.string   "email",                  :default => "",     :null => false
+    t.string   "encrypted_password",     :default => "",     :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -74,8 +90,8 @@ ActiveRecord::Schema.define(:version => 20130815202327) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                        :null => false
-    t.datetime "updated_at",                                        :null => false
+    t.datetime "created_at",                                 :null => false
+    t.datetime "updated_at",                                 :null => false
     t.string   "role",                   :default => "user"
   end
 
