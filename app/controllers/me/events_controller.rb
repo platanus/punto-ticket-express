1a72f48604261fa647d5294addd120d075d4cee1
Felipe Campos Clarke <felipecamposclarke@gmail.com>
2013-08-18 18:16:56 -0400

    change views and controllers to work properly with the new routes

---
+++ app/controllers/me/events_controller.rb
@@ -0,0 +1,92 @@
+require 'chronic'
+
+class Me::EventsController < ApplicationController
+  load_and_authorize_resource
+
+  # GET /events
+  # GET /events.json
+  def index
+    @events = current_user.events
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @events }
+    end
+  end
+
+  # GET /events/1
+  # GET /events/1.json
+  def show
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @event }
+    end
+  end
+
+  # GET /events/new
+  # GET /events/new.json
+  def new
+    @event = Event.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @event }
+    end
+  end
+
+  # GET /events/1/edit
+  def edit
+  end
+
+  # POST /events
+  # POST /events.json
+  def create
+    # publishing or saving
+    params[:event][:is_published] = params[:publish] ? true : false
+
+    # prepare event params
+    # parsing dates and times from Javascript to Rails
+    params[:event][:start_time] = Chronic.parse "#{params[:start_date]} #{params[:start_time]}"
+    params[:event][:end_time] = Chronic.parse "#{params[:end_date]} #{params[:end_time]}"
+
+    # creates the object and reference the current user
+    @event = current_user.events.build(params[:event])
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to [:me, @event], notice: 'Event was successfully created.' }
+        format.json { render json: @event, status: :created, location: @event }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /events/1
+  # PUT /events/1.json
+  def update
+    @event = Event.find(params[:id])
+
+    respond_to do |format|
+      if @event.update_attributes(params[:event])
+        format.html { redirect_to [:me, @event], notice: 'Event was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /events/1
+  # DELETE /events/1.json
+  def destroy
+    @event.destroy
+
+    respond_to do |format|
+      format.html { redirect_to events_url }
+      format.json { head :no_content }
+    end
+  end
+end
