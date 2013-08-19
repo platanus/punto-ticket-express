514cd3ef3ce16904733d0a0645a92f2a5f02dbb4
Jaime Bunzli <jpbunzli@gmail.com>
2013-08-19 11:02:26 -0400

    removes adding components to path (bower)

--- config/application.rb
+++ config/application.rb
@@ -56,9 +56,6 @@ module PuntoTicketExpress
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    # Include components folder por assets (bower)
-    config.assets.paths << Rails.root.join("vendor", "assets", "components")
-
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
