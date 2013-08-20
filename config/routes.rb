c7a8e90acb603f2a604b6c14e7ed0adf24ed914b
Felipe Campos Clarke <felipecamposclarke@gmail.com>
2013-08-20 19:58:22 -0400

    adds Nesting routes and the scope me

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,25 @@ PuntoTicketExpress::Application.routes.draw do
   ActiveAdmin.routes(self)
 
   #RESOURCES
-  resources :events do
+  resources :events, only: [:show, :update, :destroy] do
     member do
       get :participants
     end
+    resources :tickets
   end
 
-  resources :tickets
+  scope :path => '/me' do
+    resources :events, only: [:index, :new, :create, :edit] do
+      member do
+        get :admin, to: 'events#dashboard'
+      end
+    end
+  end
 
   # CUSTOM PAGES
   get '/features', to: 'features#index'
   get '/how_works', to: 'how_works#index'
 end
+
+
+
