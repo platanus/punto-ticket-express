67886605cc58b92ed20c4d9b127963f3ed50c509
Leandro Segovia <ldlsegovia@gmail.com>
2013-08-21 17:46:08 -0300

    Relating producer with user on producer creation

--- app/controllers/producers_controller.rb
+++ app/controllers/producers_controller.rb
@@ -1,6 +1,12 @@
 class ProducersController < InheritedResources::Base
   load_and_authorize_resource
 
+  def create
+    @producer = Producer.new(params[:producer])
+    @producer.users << current_user
+    create!
+  end
+
   protected
     def collection
       @producers = current_user.producers
