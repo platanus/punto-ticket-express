e2c87259789598f3cfb22357636ca7b40aa79378
Felipe Campos Clarke <felipecamposclarke@gmail.com>
2013-08-30 10:13:44 -0400

    creates new transaction view

--- app/helpers/puntopagos/transactions_helper.rb
+++ app/helpers/puntopagos/transactions_helper.rb
@@ -1,2 +1,9 @@
 module Puntopagos::TransactionsHelper
+	def calculate_price ticket_type
+		ticket_type['price'].to_i * ticket_type['qty'].to_i
+	end
+
+	def calculate_total ticket_types
+		ticket_types.inject(0) {|sum, hash| sum + (hash['price'].to_i * hash['qty'].to_i) }
+	end
 end
