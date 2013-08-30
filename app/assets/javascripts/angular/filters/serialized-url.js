75d3db070279d134d6993e02d3cfda50f0c4bfcd
Felipe Campos Clarke <felipecamposclarke@gmail.com>
2013-08-30 10:12:19 -0400

    add prefix to serailizedUrl

--- app/assets/javascripts/angular/filters/serialized-url.js
+++ app/assets/javascripts/angular/filters/serialized-url.js
@@ -1,19 +1,22 @@
 // serializes an javascript object so it can be sent through to a url and concatenated with a url
 angular.module('puntoTicketApp.filters', []).filter('serailizedUrl', function() {
-  return function(obj, url) {
-  	url = url || "";
+	// @obj (required)	  - object to be serialized
+	// @url (optional) 	  - url to the which you concatenate the serialized object
+	// @prefix (optional) - prefijo para envolver el objeto
+  	return function(obj, url, prefix) {
+  		url = url || "";
 
-  	var serialize = function(obj, prefix) {
-    	var str = [];
-    	for(var p in obj) {
-        	var k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
-        	str.push(typeof v == "object" ?
-            	serialize(v, k) :
-            	encodeURIComponent(k) + "=" + encodeURIComponent(v));
-    	}
-    	return str.join("&");
-	};
+  		var serialize = function(obj, prefix) {
+    		var str = [];
+    		for(var p in obj) {
+        		var k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
+        		str.push(typeof v == "object" ?
+            		serialize(v, k) :
+            		encodeURIComponent(k) + "=" + encodeURIComponent(v));
+    		}
+    		return str.join("&");
+		};
 
-    return url + '?' + serialize(obj);
-  };
+		return url + '?' + serialize(obj, prefix);
+  	};
 });
