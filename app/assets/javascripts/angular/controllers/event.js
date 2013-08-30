000e45a6550af710f5d0b81adda1cdc8ab46ad1d
Felipe Campos Clarke <felipecamposclarke@gmail.com>
2013-08-30 10:10:09 -0400

    removes and validates the fields of the array before being go to the next page

--- app/assets/javascripts/angular/controllers/event.js
+++ app/assets/javascripts/angular/controllers/event.js
@@ -51,12 +51,31 @@ angular.module('puntoTicketApp.controllers')
     $scope.init = function(ticketTypes, url) {
       // eliminates unnecessary attributes
       _.each(ticketTypes, function(ticketType){
+        // set default select option
+        ticketType.qty = 0;
+        // rremove attrs
         delete ticketType.created_at
         delete ticketType.updated_at
       });
 
       $scope.ticketTypes = ticketTypes;
       $scope.actionUrl = url;
+    };
+
+    // removes and validates the fields of the array before being sent to the next page
+    $scope.sendTicket = function($event) {
+
+      // removes all ticket_types that have no amount
+      var ticketTypes = _.filter($scope.ticketTypes, function(t){ return t.qty && t.qty > 0 });
+
+      // if all tickets are equal to zero is sent a warning
+      if(_.size(ticketTypes) == 0) {
+        // default action of the event will not be triggered
+        $event.preventDefault()
+        alert('No puede comprar 0 tickets!')
+      }
+      else
+        $scope.ticketTypes = ticketTypes;
     };
   }
 ]);
