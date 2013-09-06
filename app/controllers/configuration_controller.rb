c51eb40724a39d5768c4afe9a09968f86cda2768
Leandro Segovia <ldlsegovia@gmail.com>
2013-09-06 19:33:58 -0300

    configuration sidebar

--- app/controllers/configuration_controller.rb
+++ app/controllers/configuration_controller.rb
@@ -1,13 +1,16 @@
 class ConfigurationController < ApplicationController
   def account
     authorize! :config, :account
+    @user = current_user
   end
 
   def producers
     authorize! :config, :producers
+    @producers = current_user.producers
   end
 
   def transactions
     authorize! :config, :transactions
+    @transactions = current_user.transactions
   end
 end
