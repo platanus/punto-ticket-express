8351ecc178672c5aaa4e7a4583e9f08540034a68
Leandro Segovia <ldlsegovia@gmail.com>
2013-09-09 18:21:56 -0300

    Show view for producer resource

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
-def nav_link(link_text, link_path)
-  class_name = current_page?(link_path) ? 'active' : ''
-
-  content_tag(:li, :class => class_name) do
-    link_to link_text, link_path
+  def back_btn
+    link_to t("buttons.back"), request.referrer, :class => 'btn'
   end
-end
+
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
 end
