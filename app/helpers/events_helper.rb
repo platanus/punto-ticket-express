75788201b2e2cf1825a65baee47bd9f3e2ff806d
Felipe Campos Clarke <felipecamposclarke@gmail.com>
2013-09-12 18:27:24 -0300

    disables the navbar elements instead of hide

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -8,4 +8,10 @@ module EventsHelper
     event.new_record? ? new_event_path : edit_event_path
   end
 
+  def disabled_class
+  	# !@event.is_published ? 'disabled' : ''
+  	@event.new_record? ? 'disabled' : ''
+  end
+
+
 end
