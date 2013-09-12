eb7136029d6c56ad6bccbde8d59423738c207ece
Felipe Campos Clarke <felipecamposclarke@gmail.com>
2013-09-12 19:03:37 -0300

    adds helper function to events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -13,5 +13,14 @@ module EventsHelper
   	@event.new_record? ? 'disabled' : ''
   end
 
+  def attr_is_checked(attr_name, is_required)
+  	attr = @event.data_to_collect.detect { |h| h[:name].to_s == attr_name.to_s}
+  	return false if attr.nil? or attr.empty?
+  	if is_required
+  		return attr[:required]
+  	else
+  		return attr[:required] == false
+  	end
+  end
 
 end
