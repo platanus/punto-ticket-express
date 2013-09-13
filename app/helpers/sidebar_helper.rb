c8a177adffd8a3f23401852f5e72424175aab2a1
Leandro Segovia <ldlsegovia@gmail.com>
2013-09-13 15:43:49 -0300

    Sidebar helper added to manage li.active class

---
+++ app/helpers/sidebar_helper.rb
@@ -0,0 +1,24 @@
+module SidebarHelper
+
+  def active_if_account
+    if params[:controller] == "configuration" and
+      (params[:action] == "account" or params[:action] == "update_account")
+      return "active"
+    end
+  end
+
+  def active_if_transaction
+    if params[:controller].include? "transactions" or
+      params[:action] == "transactions" or
+      params[:controller].include? "tickets"
+      return "active"
+    end
+  end
+
+  def active_if_producer
+    if params[:controller].include? "producers" or
+      params[:action] == "producers"
+      return "active"
+    end
+  end
+end
