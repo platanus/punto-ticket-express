15cf2e03e14ab7308b9b46e2a9df917c0990b693
Leandro Segovia <ldlsegovia@gmail.com>
2013-09-18 18:19:39 -0300

    Showing statistic for completed transactions only

--- app/presenters/events_presenter.rb
+++ app/presenters/events_presenter.rb
@@ -4,7 +4,11 @@ class EventsPresenter
 		total = 0
 		result = event.ticket_types.inject([]) do |result, ticket_type|
 			type_data = {name: ticket_type.name, tickets_count: 0, total: 0}
-			type_data[:tickets_count] = event.tickets.where(["tickets.ticket_type_id = ?", ticket_type.id]).count
+			query = event.tickets
+			query = query.joins([:transaction])
+			query = query.where(["tickets.ticket_type_id = ?", ticket_type.id])
+			query = query.where(["transactions.payment_status = ?", PTE::PaymentStatus.completed])
+			type_data[:tickets_count] = query.count
 			type_data[:total] = type_data[:tickets_count] * ticket_type.price
 			total += type_data[:total]
 			result << type_data
