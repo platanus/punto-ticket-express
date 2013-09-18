007388d3c94c283a90cba43232747ce01604c116
Leandro Segovia <ldlsegovia@gmail.com>
2013-09-18 14:29:46 -0300

    User profile validations added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def current_user_email
+    return @current_user_email if @current_user_email
+    @current_user_email =  User.find_by_id(current_user.id).email
+  end
   def nav_link(link_text, link_path)
     class_name = current_page?(link_path) ? 'active' : ''
 
