de928e899fb121082df9eafa845f480bf8dbbf6d
Felipe Campos Clarke <felipecamposclarke@gmail.com>
2013-09-27 11:01:17 -0300

    improves the styles of the static navbar

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -10,7 +10,7 @@ $().ready(function(){
 		$(window).scroll(function() {
 			if($(window).scrollTop() > 58) {
 				$('.persistent-header').addClass('navbar-fixed-top');
-				$('body').css('paddingTop', '59px');
+				$('body').css('paddingTop', '79px');
 			}else{
 				$('.persistent-header').removeClass('navbar-fixed-top');
 				$('body').css('paddingTop', '0px');
