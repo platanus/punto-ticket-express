a841522c3a706f6595bdc544ab9c69712d37c14b
Felipe Campos Clarke <felipecamposclarke@gmail.com>
2013-10-03 13:04:37 -0300

    in the index of events, the links change depending on the state

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -7,7 +7,19 @@ module EventsHelper
   	@event.new_record? ? 'disabled' : ''
   end
 
-  def my_even_url(event)
-  	event.is_published ? sold_tickets_event_path(event) : edit_event_path(event)
+  def show_url_or_preview(event)
+  	if event.is_published?
+  		link_to(t("buttons.show"), event, :class => 'btn btn-mini')
+  	else
+  		link_to(t("buttons.show"), event_path(event, :preview => 'true'), :class => 'btn btn-mini')
+  	end
+  end
+
+  def analytics_or_edit_url(event)
+  	if event.is_published?
+  		link_to t("buttons.edit"), sold_tickets_event_path(event), :class => 'btn btn-mini'
+  	else
+  		link_to t("buttons.edit"), edit_event_path(event), :class => 'btn btn-mini'
+  	end
   end
 end
