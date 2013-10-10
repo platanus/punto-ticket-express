e5faf4a19232a5198e370ea1469b67021d7943ee
Jaime Bunzli <jpbunzli@gmail.com>
2013-10-09 22:42:23 -0300

    wip

--- config/application.rb
+++ config/application.rb
@@ -71,10 +71,10 @@ module PuntoTicketExpress
 
     config.before_configuration do
       I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
-      I18n.load_path += Dir[Rails.root.join('config', 'locales', 'views', '*.{rb,yml}')]
+      I18n.load_path += Dir[Rails.root.join('config', 'locales', 'views', '*.{rb,yml}').to_s]
       I18n.locale = I18n.default_locale = config.i18n.locale = 'es-CL'
       config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
-      config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'views', '*.{rb,yml}')]
+      config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'views', '*.{rb,yml}').to_s]
 
       # bypasses rails bug with i18n in production\
       I18n.reload!
