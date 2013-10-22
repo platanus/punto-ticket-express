438761d7813194f252434a0cf6dc2d407fdfbcb0
Leandro Segovia <ldlsegovia@gmail.com>
2013-10-22 17:38:25 -0300

    Adds PromoType Class

---
+++ lib/pte/promo_type.rb
@@ -0,0 +1,22 @@
+module PTE
+  class PromoType
+    TYPES = [:percent_discount, :amount_discount, :nx1]
+
+    PTE::PromoType::TYPES.each do |type_name|
+      self.class.class_eval do
+        define_method(type_name) do
+          type_name.to_s
+        end
+      end
+    end
+
+    def self.human_name status_name
+      I18n.t("pte.promo_type.#{status_name}", default: "Undefined promo type name" )
+    end
+
+    def self.is_valid? type_name
+      return false if type_name.nil? or type_name.empty?
+      PTE::PromoType::TYPES.include? type_name.to_sym
+    end
+  end
+end
