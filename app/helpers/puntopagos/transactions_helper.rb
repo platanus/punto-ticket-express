ff2a448fe79f93463fcaf28324314154dfacc374
Leandro Segovia <ldlsegovia@gmail.com>
2013-11-01 12:15:34 -0300

    Adds partials to change layout when nested resource form is not present. New payment methods panel added too

--- app/helpers/puntopagos/transactions_helper.rb
+++ app/helpers/puntopagos/transactions_helper.rb
@@ -13,10 +13,16 @@ module Puntopagos::TransactionsHelper
     	class: "#{payment_status}-payment-status")
 	end
 
+  def payment_button form
+    form.button(I18n.t("buttons.pay_puntopagos"),
+      :class => "btn btn-large btn-success btn-total",
+      "ng-click" => "startTransaction($event)")
+  end
+
   def payment_methods
     types = []
 
-    if Rails.env == 'production'
+    if Rails.env != 'production'
       types << ['Santander Rio', 1]
       types << ['Tarjeta Presto', 2]
       types << ['Transbank', 3]
