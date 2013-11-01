f0a7d75353ff3e7bf9e84146e389fab60e2e7c81
Leandro Segovia <ldlsegovia@gmail.com>
2013-11-01 12:57:13 -0300

    Removes old transactions/new scaffold

--- app/helpers/puntopagos/transactions_helper.rb
+++ app/helpers/puntopagos/transactions_helper.rb
@@ -4,10 +4,6 @@ module Puntopagos::TransactionsHelper
 		ticket_type['price'].to_i * ticket_type['qty'].to_i
 	end
 
-	def calculate_total ticket_types
-		ticket_types.inject(0) {|sum, hash| sum + (hash['price'].to_i * hash['qty'].to_i) }
-	end
-
 	def payment_label payment_status
   	content_tag(:span, t("pte.payment_status.#{payment_status}"),
     	class: "#{payment_status}-payment-status")
