ab9fb979050930c356b5d44f5bd3b117da492243
Felipe Campos Clarke <felipecamposclarke@gmail.com>
2013-11-05 10:36:17 -0300

    Haml to erb convertion. Changes tickets

--- app/helpers/qrcode_helper.rb
+++ app/helpers/qrcode_helper.rb
@@ -5,16 +5,17 @@ module QrcodeHelper
 		qr = RQRCode::QRCode.new(text)
 		sizeStyle = "width: #{size}px; height: #{size}px;"
 
-		haml_tag :table, class: "qrcode pull-right" do
+		content_tag :table, class: "qrcode pull-right" do
 			qr.modules.each_index do |x|
-				haml_tag :tr
-				qr.modules.each_index do |y|
-					if qr.dark? x, y
-						haml_tag :td, class: "black", style: sizeStyle
-					else
-						haml_tag :td, class: "white", style: sizeStyle
+				concat(content_tag(:tr) do
+					qr.modules.each_index do |y|
+						if qr.dark? x, y
+							concat content_tag(:td, nil, class: "black", style: sizeStyle)
+						else
+							concat content_tag(:td, nil, class: "white", style: sizeStyle)
+						end
 					end
-				end
+				end)
 			end
 		end
 	end
--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,8 +1,8 @@
 module TicketsHelper
   def printable_ticket_label label, value
-    haml_tag :div, class: "printable-label-wrapper row-fluid" do
-      haml_tag :div, label.capitalize + ": ", class: "printable-label span2"
-      haml_tag :div, value.capitalize, class: "printable-value span10"
+    content_tag :div, class: "printable-label-wrapper row-fluid" do
+      concat(content_tag :div, label.capitalize + ": ", class: "printable-label span2")
+      concat(content_tag :div, value.capitalize, class: "printable-value span10")
     end
   end
 end
