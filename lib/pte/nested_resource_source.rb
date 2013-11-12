03a7ac5bc20575b6dfa99497f747f80586499c05
Leandro Segovia <ldlsegovia@gmail.com>
2013-11-12 14:58:36 -0300

    Bug fixed: Restore all option form nested_resource source. Removed only from view

--- lib/pte/nested_resource_source.rb
+++ lib/pte/nested_resource_source.rb
@@ -1,6 +1,6 @@
 module PTE
   class NestedResourceSource
-    TYPES = [:transaction, :tickets]
+    TYPES = [:all, :transaction, :tickets]
 
     PTE::NestedResourceSource::TYPES.each do |type_name|
       self.class.class_eval do
@@ -14,8 +14,12 @@ module PTE
       I18n.t("pte.nested_resource_source.#{status_name}", default: "Undefined source" )
     end
 
-    def self.types_to_a
-      TYPES.map {|type| [human_name(type.to_s), type]}
+    def self.types_to_a exclude = []
+      types = []
+      TYPES.each do |type|
+       types << [human_name(type.to_s), type] unless exclude.include? type
+      end
+      types
     end
   end
 end
