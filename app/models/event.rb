5410a4bf8aa1e0912ae31af57c5643bfcc11ebf4
Felipe Campos Clarke <felipecamposclarke@gmail.com>
2013-11-22 14:44:13 -0300

    sorts the events in descending order & not shows the finished events

--- app/models/event.rb
+++ app/models/event.rb
@@ -33,6 +33,8 @@ class Event < ActiveRecord::Base
   accepts_nested_attributes_for :ticket_types, allow_destroy: true
 
   scope :published, where(is_published: true)
+  scope :with_valid_date, where(['end_time >= ?', DateTime.now])
+  scope :desc, order("events.created_at DESC")
 
   delegate :name, to: :producer, prefix: true, allow_nil: true
   delegate :description, to: :producer, prefix: true, allow_nil: true
