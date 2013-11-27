5489268bcc37edfa4ab6ecdf733624e444dfc9ae
Felipe Campos Clarke <felipecamposclarke@gmail.com>
2013-11-27 08:13:16 -0300

    unifies the styles

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -14,14 +14,14 @@ module EventsHelper
   def show_or_preview_button(event)
     label = "buttons.preview"
     label = "buttons.show" if event.is_published?
-  	link_to(t(label), event, :class => 'btn btn-mini btn-mini-in-line')
+  	link_to(t(label), event)
   end
 
   def statistics_or_edit_button(event)
   	if event.is_published?
-  		link_to t("buttons.statistics"), sold_tickets_event_path(event), :class => 'btn btn-mini btn-mini-in-line'
+  		link_to t("buttons.statistics"), sold_tickets_event_path(event)
   	else
-  		link_to t("buttons.edit"), edit_event_path(event), :class => 'btn btn-mini btn-mini-in-line'
+  		link_to t("buttons.edit"), edit_event_path(event)
   	end
   end
 end
