87630c997283b99250ba75b766492be0ffb981ac
Leandro Segovia <ldlsegovia@gmail.com>
2013-11-27 18:48:37 -0300

    Css adjustments - events/my_index view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,7 @@ class EventsController < ApplicationController
   skip_filter :authenticate_user!, only: :show
 
   def my_index
-    @events = current_user.events
+    @events = current_user.events.paginate(:page => params[:page], :per_page => 10)
 
     respond_to do |format|
       format.html # index.html.erb
