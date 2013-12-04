49b80001ff660905e741479f8eec3d397483b4bd
Leandro Segovia <ldlsegovia@gmail.com>
2013-12-04 10:14:46 -0300

    Bug fixed: events filtered on table but not in paginator

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   skip_filter :authenticate_user!
 
   def index
-    @events = Event.published
+    @events = Event.published.with_valid_date.order(:start_time)
   end
 end
