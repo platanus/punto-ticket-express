ae8c39445cc02adced71669ad90ee12901c966e4
Leandro Segovia <ldlsegovia@gmail.com>
2013-12-18 16:12:22 -0300

    Adds hidden fields to storage input values without number mask format.

--- app/assets/javascripts/angular/directives/currency-mask.js
+++ app/assets/javascripts/angular/directives/currency-mask.js
@@ -6,8 +6,9 @@ angular.module('puntoTicketApp.directives')
 			link: function (_scope, _element, _attrs, _ngModel) {
 				if (!_ngModel) return;
 
-				// Transform value from 999.999.999 to 999999999
+				// Removes non digit characters
 				var cleanFormat = function(_value) {
+					if(!_value) _value = 0;
 					return parseInt(_value.toString().replace(/[^0-9]+/g,''));
 				};
 
@@ -20,23 +21,38 @@ angular.module('puntoTicketApp.directives')
 					});
 				};
 
-				// Clean format before submit form to send clean values
-				$(_element).parents('form').submit( function() {
-					$(_element).val(cleanFormat($(_element).val()));
-					return true;
-				 });
+				var applyNumberFormat = function(_value) {
+					_value = cleanFormat(_value).toString();
 
-				// Sets mask after value is setted on input.
-				// This is triggered when model value is setted without user interaction.
-				// For example: on edit when value comes from server side.
-				_scope.$watch('ngModel', function() {
-					setMask();
-				});
+					var formattedNumber = [],
+						digits = _value.split(''),
+						separatorIdx = digits.length - 3;
 
+					for(var i = digits.length - 1; i >= 0; i--) {
+						formattedNumber.push(digits[i]);
+						if(separatorIdx == i && i != 0) {
+							separatorIdx -= 3;
+							formattedNumber.push('.');
+						}
+					}
+
+					return formattedNumber.reverse().join('');
+				};
+
+				// This is triggered when user interacts with text input
 				// Transform input (view) value from 999.999.999 to model value 999999999
 				_ngModel.$parsers.unshift(function (_value) {
 					return cleanFormat(_value);
 				});
+
+				// This is triggered when model value is setted without user interaction.
+				// For example: on edit when value comes from server side.
+				// Transform model value from 999999999 to view value 999.999.999
+				_ngModel.$formatters.unshift(function(_value) {
+					return applyNumberFormat(_value);
+				});
+
+				setMask();
 			}
 		};
 	}]);
