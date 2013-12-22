e73150a3d82f268ce859acc5af8d971f549e1c05
Leandro Segovia <ldlsegovia@gmail.com>
2013-12-21 22:18:27 -0300

    Removes useless directive

--- app/assets/javascripts/angular/directives/remove-nested-attr.js
+++
@@ -1,9 +0,0 @@
-angular.module('puntoTicketApp.directives').directive('removeNestedAttribute', function() {
-    return {
-        link: function (scope, element, attrs) {
-            element.on("click", function() {
-                element.parents("tr").hide();
-            });
-        }
-    };
-});
