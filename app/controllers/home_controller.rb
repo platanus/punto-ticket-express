cdc64635e734fc1102a23b8660dc313589d200eb
Leandro Segovia <ldlsegovia@gmail.com>
2014-01-22 18:03:07 -0300

    Showing only published events with defined publish range dates on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   skip_filter :authenticate_user!
 
   def index
-    @events = Event.on_sale.order(:start_time)
+    @events = Event.on_sale.in_publish_range.order(:start_time)
   end
 end
 
