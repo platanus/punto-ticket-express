7f128ae29ee7f56769bdbb64b6627c58ea18f931
Leandro Segovia <ldlsegovia@gmail.com>
2014-01-23 12:29:23 -0300

    Removes useless specs

--- spec/decorators/transaction_decorator_spec.rb
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-require 'spec_helper'
-
-describe TransactionDecorator do
-  let(:transaction) { Transaction.new.extend TransactionDecorator }
-  subject { transaction }
-  it { should be_a Transaction }
-end
