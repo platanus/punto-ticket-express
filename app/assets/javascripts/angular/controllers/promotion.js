62cb784108154c8f7447465760c19bd76dd7d76a
Felipe Campos Clarke <felipecamposclarke@gmail.com>
2014-01-28 01:15:06 -0300

    initializes models when edit promotions form

--- app/assets/javascripts/angular/controllers/promotion.js
+++ app/assets/javascripts/angular/controllers/promotion.js
@@ -1,12 +1,26 @@
 angular.module('puntoTicketApp.controllers')
   .controller('PromotionNewCtrl', ['$scope', function ($scope) {
-    $scope.init = function(_promotion, _promotable, _promoTypes) {
+
+    var loadPromationObject = function(_promotion) {
       rails_date_format = "MM-DD-YYYY";
-      $scope.promotion = {};
-      $scope.promotion.startDate = moment(_promotion.start_date, rails_date_format);
-      $scope.promotion.endDate = moment(_promotion.end_date, rails_date_format);
+
+      $scope.promotion = {
+        id: _promotion.id,
+        name: _promotion.name,
+        startDate: moment(_promotion.start_date, rails_date_format),
+        endDate: moment(_promotion.end_date, rails_date_format),
+        limit: _promotion.limit,
+        activation_code: _promotion.activation_code,
+        promotable: _.findWhere($scope.promotables, {id: _promotion.promotable_id}),
+        promotionType: _.findWhere($scope.promotionTypes, {id: _promotion.promotion_type}),
+        promotionTypeConfig: _promotion.promotion_type_config
+      };
+    };
+
+    $scope.init = function(_promotion, _promotable, _promoTypes) {
       $scope.promotables = _promotable;
       $scope.promotionTypes = _promoTypes;
+      loadPromationObject(_promotion);
     };
   }
 ]);
