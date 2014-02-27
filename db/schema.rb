dfaee7e47c0f9f8b986bd60cfdc53c87f56f70ef
Camila Parot <camila.parot@gmail.com>
2014-02-27 14:25:39 -0300

    adds the color themes for the events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140127154500) do
+ActiveRecord::Schema.define(:version => 20140128142227) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
