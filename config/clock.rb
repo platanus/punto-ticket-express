31dda8c7313576010104ece2c47bbaabca4bf7d1
Juan Ignacio Donoso <jidonoso@gmail.com>
2017-05-24 18:52:23 -0400

    chore(): adds commented tasks to clock.rb

--- config/clock.rb
+++ config/clock.rb
@@ -5,7 +5,19 @@ require_relative './environment'
 module Clockwork
   # Example
   #
-  # every(5.minutes, 'ScheduledJob') do
-  #   ScheduledJob.perform_later
+  # every 50.minutes do
+  #   rake "pte:cron:deactivate_old_pending_transactions", :environment => :development
+  # end
+
+  # every 5.minutes do
+  #   rake "pte:cron:deactivate_old_pending_transactions", :environment => :production
+  # end
+
+  # every 20.minutes do
+  #   rake "pte:cron:clean_old_participant_files", :environment => :development
+  # end
+
+  # every 50.minutes do
+  #   rake "pte:cron:clean_old_participant_files", :environment => :production
   # end
 end
